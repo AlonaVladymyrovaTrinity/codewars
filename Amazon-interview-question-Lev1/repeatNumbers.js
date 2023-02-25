1aec5f164e1b678b34bf9b80d4076e0a8812a82e
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-02-24 16:42:57 -0800

    Code refactoring has been made to my Amazon interview solution

--- Amazon-interview-question-Lev1/repeatNumbers.js
+++ Amazon-interview-question-Lev1/repeatNumbers.js
@@ -1,7 +1,7 @@
 let repeatNumbers = (arr) => {
-  let newArr = arr.sort();
   let repNum = 0;
   let repNumArr = [];
+  arr.sort();
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === arr[i + 1] && repNum !== arr[i - 1]) {
       repNumArr.push(arr[i + 1]);
@@ -11,5 +11,5 @@ let repeatNumbers = (arr) => {
   return repNumArr;
 };
 
-//console.log(repeatNumbers([1, 3, 4, 3, 2, 1]));
-console.log(repeatNumbers([1, 1, 1, 2]));
+console.log(repeatNumbers([1, 3, 4, 3, 2, 1]));
+//console.log(repeatNumbers([1, 1, 1, 2]));
